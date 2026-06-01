69f0c61b9f5280c80a65732837b9b386cf013217
PRIYANGSU BANIK <136341993+NxtNinja@users.noreply.github.com>
2026-06-02 00:59:08 +0530

    chore: initialize project architecture and core providers

---
+++ src/providers/app-provider.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { ReactNode } from "react";
+import { QueryProvider } from "./query-provider";
+import { ThemeProvider } from "./theme-provider";
+
+interface AppProviderProps {
+  children: ReactNode;
+}
+
+export function AppProvider({ children }: AppProviderProps) {
+  return (
+    <ThemeProvider>
+      <QueryProvider>{children}</QueryProvider>
+    </ThemeProvider>
+  );
+}
---
+++ src/providers/query-provider.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode, useState } from "react";
+import { makeQueryClient } from "@/lib/query-client";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+interface QueryProviderProps {
+  children: ReactNode;
+}
+
+export function QueryProvider({ children }: QueryProviderProps) {
+  const [queryClient] = useState(() => makeQueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
